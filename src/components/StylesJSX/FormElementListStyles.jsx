d739c97d637917f38c8630be6db22b5d8b5c3fed
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2023-12-26 18:23:22 +0200

    fix

--- src/components/StylesJSX/FormElementListStyles.jsx
+++ src/components/StylesJSX/FormElementListStyles.jsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const FormDiv = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 export const FormElementDiv = styled.div`
   display: flex;
